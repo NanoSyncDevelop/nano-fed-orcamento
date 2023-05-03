cbd5e3071b475d733a3c56c6a90b33375a84a18e
Martin Hartfelder <contato@nanosync.dev>
2023-05-03 08:15:02 -0300

    [fix] Correção de boas práticas

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,13 +1,11 @@
 window.onload = function() {
+    emailjs.init("a5rbV4xMUmfpeQTh6");
+
     if (localStorage.getItem('MAILED')) {
         document.getElementById('alert-error-01').style.display = 'block';
         document.getElementById('submit-button').disabled = 'true';
     }
 };
-
-(function(){
-    emailjs.init("a5rbV4xMUmfpeQTh6");
-})();
 
 function sendMail() {
     const form = document.querySelector('form');
@@ -18,13 +16,12 @@ function sendMail() {
             localStorage.setItem('MAILED', 'YES')
             emailjs.sendForm('service_qsyt7js', 'template_efs7zzr', '#contact-form')
                 .then(function(response) {
-                    console.log('SUCCESS!', response.status, response.text);
+                    document.getElementById('alert-success-01').style.display = 'block';
                 }, function(error) {
-                    console.log('FAILED...', error);
+                    document.getElementById('alert-error-01').textContent = 'Ocorreu um erro desconhecido, tente novamente em breve!';
                 });
             form.reset();
             event.preventDefault();
-            document.getElementById('alert-success-01').style.display = 'block';
         }
     });
 }
