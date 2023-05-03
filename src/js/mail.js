88d4e9e2a0390d5a82fbdfc5355cbf1170d4327f
Martin Hartfelder <contato@nanosync.dev>
2023-05-03 08:07:45 -0300

    [fix] Correção de alertas

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+    if (localStorage.getItem('MAILED')) {
+        document.getElementById('alert-error-01').style.display = 'block';
+        document.getElementById('submit-button').disabled = 'true';
+    }
+};
+
 (function(){
     emailjs.init("a5rbV4xMUmfpeQTh6");
 })();
@@ -6,7 +13,6 @@ function sendMail() {
     const form = document.querySelector('form');
     form.addEventListener('submit', (event) => {
         if (localStorage.getItem('MAILED')) {
-            document.getElementById('alert-error-01').style.display = 'block';
             event.preventDefault();
         } else {
             localStorage.setItem('MAILED', 'YES')
@@ -17,6 +23,7 @@ function sendMail() {
                     console.log('FAILED...', error);
                 });
             form.reset();
+            event.preventDefault();
             document.getElementById('alert-success-01').style.display = 'block';
         }
     });
