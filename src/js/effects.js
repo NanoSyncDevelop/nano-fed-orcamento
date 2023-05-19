d28660e96163d0a12c7a87fe8967b6a0fd1ba3fd
Martin Hartfelder <contato@nanosync.dev>
2023-05-19 20:09:09 -0300

    [fix] Correção para mobile de menu

--- src/js/effects.js
+++ src/js/effects.js
@@ -1,8 +1,3 @@
-// Loading Components
-// window.onload = function() {
-//     $('#header-boilerplate').load('src/components/header.html');
-// };
-
 window.addEventListener('scroll', function() {
     if (window.scrollY > 80) {
         if (window.screen.height < 900) {
